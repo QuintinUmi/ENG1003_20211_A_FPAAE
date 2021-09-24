349ec6909fc2a4fac3a5a038396326ebcfa766a7
Qin Qijun <qqj030212@gmail.com>
2021-09-24 13:12:16 +0800

    ok

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import math
 print ("Hello World")
 s = "Hello World"
+print (s[0:5])
+print (s[-5:-1])
+alist = ['a', "Qin QJ", 1, 1.5]
+print (alist)
+print (alist[1])
 
-print (s[0])
+k = not(5**20 > 4**21)
+print(k)
